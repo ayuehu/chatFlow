c7b58b9e14cec3947cd572997007f4cc8bafdf68
lingshu.hy <lingshu.hy@alibaba-inc.com>
2025-02-23 04:19:33 +0800

    Initial Commit

---
+++ ShareQuestionsTests/ShareQuestionsTests.swift
@@ -0,0 +1,17 @@
+//
+//  ShareQuestionsTests.swift
+//  ShareQuestionsTests
+//
+//  Created by ayue on 2025/2/23.
+//
+
+import Testing
+@testable import ShareQuestions
+
+struct ShareQuestionsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
